09e9acaf1f5a7345d44cbc4270dfec602b9706f7
estebiart <estebiart@gmail.com>
2024-10-07 06:33:00 -0500

    feat: :sparkles: configure the handle of the user with Jwt
    This commit adds a middleware class `Authenticate` that handles user authentication via JSON Web Tokens (JWT) in the Laravel application. The middleware attempts to parse the incoming request's token and authenticate the user. If the token is valid and the user is found, the request is allowed to proceed. If the token is missing or invalid, appropriate error responses are returned, such as a 404 status when the user is not found or a 401 status when the token is not provided. This middleware is crucial for ensuring secure access to protected routes within the application.

---
+++ backendSM/app/Http/Middleware/Authenticate.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class Authenticate
+{
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['error' => 'User not found'], 404);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Token not provided'], 401);
+        }
+
+        return $next($request);
+    }
+}
+
