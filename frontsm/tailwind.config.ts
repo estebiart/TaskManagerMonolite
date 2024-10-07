41958c4b593d142fac2d8cc179aa762ec8b8b738
estebiart <estebiart@gmail.com>
2024-10-07 06:09:26 -0500

    feat: :sparkles: initialize Next.js frontend with Tailwind CSS and initial package setup
    This commit initializes the frontend of the monolithic application using Next.js. It includes the setup of Tailwind CSS for styling, providing a utility-first approach for building responsive UIs. Additionally, the basic packages required for development have been installed and configured, including necessary dependencies for the smooth integration of Next.js with Tailwind. This setup forms the foundation for the frontend development and will streamline the process of building scalable and maintainable components.

---
+++ frontsm/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E3A8A', 
+        secondary: '#FBBF24', 
+        accent: '#EF4444', 
+        background: '#F9FAFB', 
+        text: '#111827', 
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        mono: ['Menlo', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
